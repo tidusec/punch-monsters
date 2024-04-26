9333a61fef6c62a499508e6cc5201951f8a8032a
Timothy Dudley <timothydudley@tutanota.com>
2024-04-26 14:47:19 +0200

    enemyfighting

--- src/Server/Services/EnemyFightingService.lua
+++ src/Server/Services/EnemyFightingService.lua
@@ -18,35 +18,82 @@ local EnemyFightingService = Knit.CreateService {
 function EnemyFightingService:KnitStart(): nil
     self._data = Knit.GetService("DataService")
 	self._enemies = require(ReplicatedStorage.Templates.EnemiesTemplate)
+	self._rebirths = Knit.GetService("RebirthService")
+	self._boosts = Knit.GetService("BoostService")
+	self._gamepass = Knit.GetService("GamepassService")
     self.memory = {}
-	return
-end
 
-function EnemyFightingService:KnitInit(): nil
-	self._playersJoinedAt = {}
-
-	Players.PlayerAdded:Connect(function(player: Player): nil
-		self._playersJoinedAt[player.UserId] = tick()
-		return
+	Players.PlayerAdded:Connect(function(player: Player)
+		self.memory[player.UserId] = {
+			entered = false,
+			fighting = false,
+			health = 100,
+			boss = "",
+			bosshealth = 100,
+		}
 	end)
-	Players.PlayerRemoving:Connect(function(player: Player): nil
-		self._playersJoinedAt[player.UserId] = nil
-		return
+
+	Players.PlayerRemoving:Connect(function(player: Player)
+		self.memory[player.UserId] = nil
 	end)
 
 	return
 end
 
 function EnemyFightingService:Enter(player: Player, boss: string): string
-	self.memory[player.UserId] = boss
+	if self.memory[player.UserId].entered then return end
+	if self._enemies[boss] then
+		self.memory[player.UserId].entered = true
+		self.memory[player.UserId].boss = boss
+		self.memory[player.UserId].bosshealth = self._enemies[boss].health
+		return
+	end
 end
 
 function EnemyFightingService:StartFight(player: Player, boss: string): string
-	self.memory[player.UserId] = boss
+	if not self.memory[player.UserId].entered then return end
+	if self.memory[player.UserId].fighting then return end
+	if self.memory[player.UserId].boss == boss then
+		self.memory[player.UserId].fighting = true
+		return
+	end
 end
 
 function EnemyFightingService:Attack(player: Player): string
-	return 100, 250
+	if not self.memory[player.UserId].fighting then return end
+	self.memory[player.UserId].bosshealth -= self._data:GetTotalStrength(player)
+	if self.memory[player.UserId].bosshealth <= 0 then
+		if self._enemies[self.memory[player.UserId].boss].Boss then
+			local bossmap = "Map"..self._enemies[self.memory[player.UserId].boss].Map
+			self._data:AddDefeatedBoss(player, bossmap)
+		end
+		
+		self:AddWin(player)
+		self:ClearData(player)
+		return
+	elseif self.memory[player.UserId].health <= 0 then
+		self:ClearData(player)
+		return
+	else
+		return self.memory[player.UserId].bosshealth, self.memory[player.UserId].health
+	end
+end
+
+function EnemyFightingService:AddWin(player: Player): string
+	local hasDoubleWins = self._gamepass:DoesPlayerOwn(player, "2x Wins")
+	local hasWinsBoost = self._boosts:IsBoostActive(player, "2xWins")
+	local gamepassMultiplier: number = (if hasDoubleWins then 2 else 1) * (if hasWinsBoost then 2 else 1)
+	local rebirthMultiplier: number = self._rebirths:GetBoost(player, "Wins")
+	self._data:IncrementValue("Wins", (self :: any)._enemyTemplate.Wins * rebirthMultiplier * gamepassMultiplier)
+	return
+end
+
+function EnemyFightingService:ClearData(player: Player): string
+	self.memory[player.UserId].fighting = false
+	self.memory[player.UserId].boss = ""
+	self.memory[player.UserId].bosshealth = 100
+	self.memory[player.UserId].entered = false
+	return
 end
 
 function EnemyFightingService.Client:Enter(player: Player, boss: string): number
--- src/Shared/Templates/EnemiesTemplate.lua
+++ src/Shared/Templates/EnemiesTemplate.lua
@@ -5,69 +5,87 @@ local module = {
 	
 	Nerd = {
 		Wins = 1,
-		Strength = 100
+		Strength = 100,
+		Boss = false
 	},
 	Businessman = {
 		Wins = 25,
-		Strength = 1200
+		Strength = 1200,
+		Boss = false
 	},
 	Bodybuilder = {
 		Wins = 75,
-		Strength = 8700
+		Strength = 8700,
+		Boss = false
 	},
 	Hitman = {
 		Wins = 150,
-		Strength = 18000
+		Strength = 18000,
+		Boss = false
 	},
 	Powerlifter = {
 		Wins = 3500,
-		Strength = 45000
+		Strength = 45000,
+		Boss = true,
+		Map = 1,
 	},
 	
 	-- Map2
 	
 	Mummy = {
 		Wins = 2000,
-		Strength = 380000
+		Strength = 380000,
+		Boss = false
 	},
 	DesertExplorer = {
 		Wins = 10000,
-		Strength = 640000
+		Strength = 640000,
+		Boss = false
 	},
 	DesertScout = {
 		Wins = 22000,
-		Strength = 11000000
+		Strength = 11000000,
+		Boss = false
 	},
 	Soldier = {
 		Wins = 50000,
-		Strength = 45000000
+		Strength = 45000000,
+		Boss = false
 	},
 	Pharaoh = {
 		Wins = 225000,
-		Strength = 120000000
+		Strength = 120000000,
+		Boss = true,
+		Map = 2,
 	},
 	
 	-- Map3
 	
 	Zombie = {
 		Wins = 4500000,
-		Strength = 400000000
+		Strength = 400000000,
+		Boss = false
 	},
 	Golem = {
 		Wins = 35000000,
-		Strength = 1200000000
+		Strength = 1200000000,
+		Boss = false
 	},
 	Skeleton = {
 		Wins = 115000000,
-		Strength = 8100000000
+		Strength = 8100000000,
+		Boss = false
 	},
 	Reaper = {
 		Wins = 375000000,
-		Strength = 25000000000
+		Strength = 25000000000,
+		Boss = false
 	},
 	Demon = {
 		Wins = 1800000000,
-		Strength = 92000000000
+		Strength = 92000000000,
+		Boss = false,
+		Map = 3,
 	},
 }
 
